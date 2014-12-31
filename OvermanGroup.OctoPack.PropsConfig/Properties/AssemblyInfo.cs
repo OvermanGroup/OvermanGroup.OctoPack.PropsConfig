7aa38814d9faa5b21c1aced95453b7b1078dcb2f
Marcin Polewski <marcin.polewski@gmail.com>
2014-12-31 01:42:38 -0600

    Fixed grammar error in description.

--- OvermanGroup.OctoPack.PropsConfig/Properties/AssemblyInfo.cs
+++ OvermanGroup.OctoPack.PropsConfig/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OvermanGroup.OctoPack.PropsConfig")]
 [assembly: AssemblyProduct("OvermanGroup.OctoPack.PropsConfig")]
-[assembly: AssemblyDescription("This NuGet package that adds an 'OvermanGroup.OctoPack.props' file to your project so that you can easily configure MSBuild properties for OctoPack within your Visual Studio project.")]
+[assembly: AssemblyDescription("This NuGet package adds an 'OvermanGroup.OctoPack.props' file to your project so that you can easily configure MSBuild properties for OctoPack within your Visual Studio project.")]
 
 [assembly: AssemblyCompany("Overman Group, LLC.")]
 [assembly: AssemblyCopyright("Copyright © 2014 Overman Group, LLC.")]
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ff02420b-1c88-4940-83f2-8d44b35a3842")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
